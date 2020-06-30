a78d2fda13daf36fa7b37ba3284f477c4749fddb
Harry McCarthy <d4m3on@outlook.com>
2020-07-01 09:02:28 +1200

    Update the auth service to update the token in the background

--- src/app/shared/core/services/auth.service.ts
+++ src/app/shared/core/services/auth.service.ts
@@ -1,29 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MsalService, BroadcastService } from '@azure/msal-angular';
+import { UserAgentApplication } from 'msal';
+import { StringDict } from 'msal/lib-commonjs/MsalTypes';
+
+
+const idTokenScope = { scopes: ['5efe6947-d960-4808-84f9-6454a37be0b6'] }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public msalService: MsalService, private broadcastService: BroadcastService) {}
+  constructor(public msalService: MsalService, private broadcastService: BroadcastService) {
+    msalService.acquireTokenSilent(idTokenScope);
+  }
 
-  msLogin(): void{
+  msLogin(): void {
     this.msalService.loginRedirect({
       extraScopesToConsent: ["user.read", "openid", "profile"]
     });
   }
-  msLogout(): void{
+  msLogout(): void {
     this.msalService.logout();
   }
-  isLoggedIn(): boolean{
-    if(this.msalService.getAccount() === null){
+  isLoggedIn(): boolean {
+    if (this.msalService.getAccount() === null) {
       return false;
     }
     return true;
   }
-  msBroadcast(){
-    
+  msBroadcast() {
+
   }
 }
